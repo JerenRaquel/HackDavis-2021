3c488781a6df95f1927e839e76a51810bd5f9f5b
Jeren Raquel <jeraquel101@gmail.com>
2021-01-16 15:59:37 -0800

    Added RandomEventSystem

---
+++ Assets/Scripts/RandomEventSystem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class RandomEvent
+{
+  public string name = "No Name";
+  [TextArea(10, 15)]
+  public string description = "";
+  public float amount = 0;
+  public RandomEvent nestedEvent = null;
+}
+
+public class RandomEventSystem : MonoBehaviour
+{
+  public RandomEvent[] events;
+
+  public float GetRandomEventAmount(RandomEvent rngEvent = null)
+  {
+    // Check if event is played
+    int chance = Random.Range(0, 1);
+    if (chance == 0)
+      return 0;
+
+    // Get event
+    RandomEvent ree;
+    if (rngEvent == null)
+      ree = events[Random.Range(0, events.Length)];
+    else
+      ree = rngEvent;
+
+    // Check for nested event
+    if (ree != null)
+      return ree.amount + GetRandomEventAmount(ree.nestedEvent);
+
+    return 0;
+  }
+}
