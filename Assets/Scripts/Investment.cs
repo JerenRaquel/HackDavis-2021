0cc78023dc39d88e364b71a5f9d98284e74f4efc
Jeren Raquel <jeraquel101@gmail.com>
2021-01-16 11:00:28 -0800

    Added Base class for the different investments

---
+++ Assets/Scripts/Investment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Investment : MonoBehaviour
+{
+  private float totalValue = 0;
+
+  public void AddFunds(float amount)
+  {
+    this.totalValue += amount;
+  }
+
+  public void RemoveFunds(float amount)
+  {
+    this.totalValue -= amount;
+    this.totalValue = this.totalValue < 0 ? 0 : this.totalValue;
+  }
+
+  public float TotalFunds => this.totalValue;
+}
