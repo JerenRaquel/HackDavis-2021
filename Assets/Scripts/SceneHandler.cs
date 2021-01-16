39c196a63290aa0d18b805b3efd7ce449a95b842
Jeren Raquel <jeraquel101@gmail.com>
2021-01-16 10:17:32 -0800

    Added SceneSystem to handle switching b/w scenes

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+  public enum SCENES { SELF = 0, TITLE, GAME }
+
+  public SCENES defaultScene;
+
+  private void Start() {
+    SwitchScene(defaultScene);
+  }
+
+  public void SwitchScene(SCENES scene)
+  {
+    bool sceneLoaded = false;
+    for (int i = 0; i < SceneManager.sceneCount; i++)
+    {
+      if(SceneManager.GetSceneAt(i) == SceneManager.GetActiveScene())
+        continue;
+      else if(SceneManager.GetSceneAt(i) == SceneManager.GetSceneByBuildIndex((int)scene))
+        sceneLoaded = true;
+      else{
+        SceneManager.UnloadSceneAsync(SceneManager.GetSceneAt(i).buildIndex);
+      }
+    }
+
+    if(sceneLoaded)
+      return;
+
+    SceneManager.LoadSceneAsync((int)scene, LoadSceneMode.Additive);
+  }
+}
