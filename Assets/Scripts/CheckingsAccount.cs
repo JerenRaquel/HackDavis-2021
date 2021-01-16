9bfc99714193e62257c5fa444f58a90334a272ed
yutao-li-306 <tonyyuli306@gmail.com>
2021-01-16 14:35:17 -0800

    Did work on SavingsAccount and CheckingsAccount

--- Assets/Scripts/CheckingsAccount.cs
+++ Assets/Scripts/CheckingsAccount.cs
@@ -1,23 +1,32 @@
 public class CheckingsAccount : Investment {
     
-    int income = 0;
+    int income;
     int expense = 0;
 
+    public CheckingsAccount(int income) {
+        this.income = income;
+    }
+
+    // when output -1, it means that the CheckingAccount has no money and the player needs to reallocate money to pay.
     public int RemoveFunds(int amount) {
         if (this.TotalFunds < amount) {
+            // 
             return -1;
         }
         this.TotalFunds -= amount;
     }
 
+    // to be used by outside functions such as events
     public void addExpenses(int expenseAmount) {
         expense += expenseAmount;
     }
 
+    // to be used by outside functions such as events and main
     public void subtractExpenses(int subtractAmount) {
         expense -= subtractAmount;
     }
 
+    // to be used by outside functions such as Assets and Education
     public void addIncome(int incomeAmount) {
         income += incomeAmount;
     }
