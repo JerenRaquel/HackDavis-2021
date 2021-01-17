8df14a2c78e1c0ed175729c82785045fef3ffbf0
yutao-li-306 <tonyyuli306@gmail.com>
2021-01-17 00:14:46 -0800

    Fixed Changes on Stock.cs

--- Assets/Scripts/CheckingsAccount.cs
+++ Assets/Scripts/CheckingsAccount.cs
@@ -1,41 +1,39 @@
 public class CheckingsAccount : Investment
 {
-
     int income;
-    int expense = 0;
+    int expense;
 
-    public CheckingsAccount(int income)
+    public CheckingsAccount()
     {
-        this.income = income;
+        income = 10000;
+        expense = 0;
     }
 
     // when output -1, it means that the CheckingAccount has no money and the player needs to reallocate money to pay.
-    public int Withdraw(int amount)
+    public void Withdraw(int amount)
     {
-        if (this.totalValue < amount)
-        {
+        //if (this.totalValue < amount)
+        //{
             // pops to another function (not coded yet) which has to purpose to allow the user to divert other funds to solve this error.
-            return -1;
-        }
+        //    return -1;
+        //}
         this.totalValue -= amount;
-        return 1;
-
     }
 
     // to be used by outside functions such as events
-    public void addExpenses(int expenseAmount)
+    public void AddExpenses(int expenseAmount)
     {
         expense += expenseAmount;
     }
 
     // to be used by outside functions such as events and main
-    public void subtractExpenses(int subtractAmount)
+    public void SubtractExpenses(int subtractAmount)
     {
         expense -= subtractAmount;
     }
 
     // to be used by outside functions such as Assets and Education
-    public void addIncome(int incomeAmount)
+    public void AddIncome(int incomeAmount)
     {
         income += incomeAmount;
     }
