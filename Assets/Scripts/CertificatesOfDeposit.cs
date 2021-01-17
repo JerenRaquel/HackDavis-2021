5a1a4c9995e18dc396f1c6c843e8adf2ec74c9c7
Jeren Raquel <jeraquel101@gmail.com>
2021-01-16 16:14:48 -0800

    Fixed errors in CDs

--- Assets/Scripts/CertificatesOfDeposit.cs
+++ Assets/Scripts/CertificatesOfDeposit.cs
@@ -1,60 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+
 public class CD
 {
-    int value;
-    int rate;
-    int year;
+  float value;
+  float rate;
+  int year;
+
+  public CD(int amount, float rate, int year)
+  {
+    this.value = amount;
+    this.rate = rate;
+    this.year = year;
+  }
+
+  public float Value { get { return this.value; } set { this.value = value; } }
+  public float Rate => this.rate;
+  public int Year { get { return this.year; } set { this.year = value; } }
 }
 
 public class CertificatesOfDeposit : Investment
 {
-    List<CD> List_CDs;
-    float[] interestRates;
+  List<CD> List_CDs;
+  float[] interestRates;
 
-    public CertificatesOfDeposit(int year, float[] interestRates)
+  public CertificatesOfDeposit(int year, float[] interestRates)
+  {
+    this.year = year;
+    this.interestRates = interestRates;
+    this.rate = interestRates[year];
+    this.List_CDs = new List<CD>();
+  }
+
+  public void Deposit(int amount, int yearOption)
+  {
+    CD item = new CD(amount, rate, yearOption);
+    List_CDs.Add(item);
+  }
+
+  public float collect(int id)
+  {
+    // this is where the error will happen where you're trying to collect an id that does not exist
+    if (List_CDs.Count <= id)
     {
-        this.year = year;
-        this.interestRates = interestRates;
-        this.rate = interestRates[year];
-        this.List_CDs = new List<CD>();
+      return -1;
     }
 
-    public void Deposit(int amount, int yearOption)
+    // if the CD has not reached maturity, collecting it will result in a penalty amount
+    float collect_value;
+    if (List_CDs[id].Year != 0)
+      collect_value = .9f * (float)List_CDs[id].Value;
+    else
+      collect_value = List_CDs[id].Value;
+
+    List_CDs.RemoveAt(id);
+    return collect_value;
+  }
+
+  public void Progression()
+  {
+    this.year++;
+    foreach (CD cd in List_CDs)
     {
-        CD item = new CD(amount, rate, yearOption);
-        List_CDs.add(item);
+      if (year != 0)
+      {
+        cd.Year--;
+        cd.Value = cd.Value + cd.Value * (1 + cd.Rate);
+      }
     }
-
-    public int collect(int id)
-    {
-        // this is where the error will happen where you're trying to collect an id that does not exist
-        if (List_CDs.size() <= id)
-        {
-            return -1;
-        }
-
-        // if the CD has not reached maturity, collecting it will result in a penalty amount
-        int collect_value;
-        if (List_CDs[id].year != 0)
-            collect_value = .9 * List_CDs[id].value;
-        else
-            collect_value = List_CDs[id].value;
-
-        List_CDs.RemoveAt(id);
-        return collect_value;
-    }
-
-    public void Progression()
-    {
-        this.year++;
-        foreach (CD cd in List_CDs)
-        {
-            if (year != 0)
-            {
-                cd.year--;
-                cd.value = cd.value + cd.value * (1 + cd.rate);
-            }
-        }
-        this.rate = interestRates[year];
-    }
+    this.rate = interestRates[year];
+  }
 
 }
