c8a19ae144756ee7f5c46da33b36813786ee9718
Jeren Raquel <jeraquel101@gmail.com>
2021-01-17 14:52:14 -0800

    Fixed some issues

--- Assets/Scripts/SavingsAccount.cs
+++ Assets/Scripts/SavingsAccount.cs
@@ -4,36 +4,36 @@ using UnityEngine;
 
 public class SavingsAccount : Investment
 {
-    // the variables SavingAccount will use.
-    private float[] interestRates;
+  // the variables SavingAccount will use.
+  public float[] interestRates;
 
-    // constructor for SavingsAccount object.
-    public SavingsAccount(float[] interestRates, int year)
-    {
-        this.totalValue = 0;
-        this.year = year;
-        this.interestRates = interestRates;
-        this.rate = interestRates[year];
-    }
+  // constructor for SavingsAccount object.
+  public SavingsAccount(float[] interestRates, int year)
+  {
+    this.totalValue = 0;
+    this.year = year;
+    this.interestRates = interestRates;
+    this.rate = interestRates[year];
+  }
 
-    // For Displaying purposes
-    // Gets a string which represents the SavingsAccount Data
-    public string DisplaySavings()
-    {
-        string result;
-        result = "Amount: " + this.totalValue + "\nInterest rate: " + this.rate;
-        return result;
-    }
+  // For Displaying purposes
+  // Gets a string which represents the SavingsAccount Data
+  public string DisplaySavings()
+  {
+    string result;
+    result = "Amount: " + this.totalValue + "\nInterest rate: " + this.rate;
+    return result;
+  }
 
-    // Progression should be called everytime the global game timer refreshes.
-    // increment year counter to math system year
-    // do simple interest on the value in SavingsAccount
-    // update the interest rate
-    public void Progression()
-    {
-        totalValue = Mathf.RoundToInt(totalValue + totalValue * rate);
-        year++;
-        this.rate = interestRates[year];
-    }
+  // Progression should be called everytime the global game timer refreshes.
+  // increment year counter to math system year
+  // do simple interest on the value in SavingsAccount
+  // update the interest rate
+  public void Progression()
+  {
+    totalValue = Mathf.RoundToInt(totalValue + totalValue * rate);
+    year++;
+    this.rate = interestRates[year];
+  }
 
 }
